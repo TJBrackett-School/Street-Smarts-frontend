6a549212953c329d2481c0e07892631d80328d32
Michael Edwards <leahcim12000@gmail.com>
2019-02-22 11:03:29 -0800

    Added registering Functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -16,7 +17,7 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  CognitoUserPool,
+  CognitoUserAttribute
+} from 'amazon-cognito-identity-js/dist/amazon-cognito-identity.min';
+import {
+  environment
+} from '../../environments/environment';
+
+
+
 
 @Component({
   selector: 'app-register',
@@ -6,10 +19,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  private userName;
+  private userEmail;
+  private userPassWord;
+  private userRepeatPassWord;
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor() {}
+
+
+  ngOnInit() {}
+
+  onUserNameChange(event: any) {
+    this.userName = event.target.value;
+    // console.log(this.userName);
+  }
+
+  onEmailChange(event: any) {
+    this.userEmail = event.target.value;
+  }
+
+  onPassChange(event: any) {
+    this.userPassWord = event.target.value;
+    // console.log(this.passWord);
+  }
+
+  onRepeatPassChange(event: any) {
+    this.userRepeatPassWord = event.target.value;
+  }
+
+
+  registerUser() {
+    if (this.userPassWord === this.userRepeatPassWord) {
+      const poolData = {
+        UserPoolId: environment.COGNITO_USER_POOL_ID, // Your user pool id here
+        ClientId: environment.COGNITO_CLIENT_ID // Your client id here
+      };
+
+      const userPool = new CognitoUserPool(poolData);
+
+      const attributeList = [];
+
+
+      const dataEmail = {
+        Name: 'email',
+        Value: this.userEmail
+      };
+
+
+      const attributeEmail = new CognitoUserAttribute(dataEmail);
+      let cognitoUser;
+      attributeList.push(attributeEmail);
+
+      userPool.signUp(this.userName, this.userPassWord, attributeList, null, function (err, result) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        cognitoUser = result.user;
+        console.log('user name is ' + cognitoUser.getUsername());
+      });
+    } else {
+      alert('Repeated password doesnt match');
+      console.log(this.userPassWord + " : " + this.userRepeatPassWord)
+    }
   }
 
 }
